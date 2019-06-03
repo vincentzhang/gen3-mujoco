fcbbcfb526824a3fafae4130883c1dfd494e2899
Vincent Zhang <vzhangca@gmail.com>
2019-06-03 17:47:12 -0600

    add the first version of gen3 arm in mujoco

---
+++ mujoco_description/disp_gen3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Displays robot Gen3 
+"""
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+from mujoco_py.modder import TextureModder
+import os
+
+model = load_model_from_path("models/GEN3.xml")
+sim = MjSim(model)
+
+viewer = MjViewer(sim)
+
+t = 0
+
+while True:
+    sim.step()
+    viewer.render()
+    t += 1
+    if t > 100 and os.getenv('TESTING') is not None:
+        break
