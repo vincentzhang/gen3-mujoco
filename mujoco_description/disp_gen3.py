62926da9b70c1ff6c473d86ddb0a0b1ee0d09fff
Laura Petrich <laura.petrich@gmail.com>
2019-07-08 13:01:35 -0600

    added robotiq gripper, currently stationary, need to add in step instructions

--- mujoco_description/disp_gen3.py
+++ mujoco_description/disp_gen3.py
@@ -6,7 +6,8 @@ from mujoco_py import load_model_from_path, MjSim, MjViewer
 from mujoco_py.modder import TextureModder
 import os
 
-model = load_model_from_path("models/GEN3.xml")
+# model = load_model_from_path("models/GEN3.xml")
+model = load_model_from_path("models/gen3_robotiq.xml")
 sim = MjSim(model)
 
 viewer = MjViewer(sim)
